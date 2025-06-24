220951c9e227515bf7bef136a2e467da01c914c2
LuisACampos7 <00122425@uca.edu.sv>
2025-06-23 23:13:54 -0600

    buenas

---
+++ mapa.h
@@ -0,0 +1,21 @@
+#include <stdafx.h>
+#include <iostream>
+#include <conio.h>
+
+using namespace System;
+using namespace std;
+
+
+#define FIL 23
+#define COL 15
+
+int mapa[FIL][COL];
+
+void imprimir_mapa();
+void copia_nivel1();
+
+int main(array<system){
+    setWindowSize(COL.FIL);
+    CursorVisible = false;
+    clear();
+}
